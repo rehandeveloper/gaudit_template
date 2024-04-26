f6f603b690fecd2530a7e9d638e4ae8ecb67eeaa
rockvns <chandrabhanvns@gmail.com>
2024-04-26 17:00:03 +0530

    teams page

---
+++ src/data/teams.js
@@ -0,0 +1,14 @@
+export const teamsData = [
+  {
+    name: "John Doe",
+    designation: "software development",
+    quote: "Proin laoreet nisi vitae pharetra mattis, etiam luctus suscipit velit vitae amatug ultricies. Augue molestie a etiam.",
+    profilePic:"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+  },
+  {
+    name: "Foo Bar",
+    designation: "Recruiter",
+    quote: "Proin laoreet nisi vitae pharetra mattis, etiam luctus suscipit velit vitae amatug ultricies. Augue molestie a etiam.",
+    profilePic:"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+  },
+];
