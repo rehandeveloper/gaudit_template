29e5de429e70421819d3866580f5a9bfa10a6dc7
rehandev <rehan.ali@cxltechnologies.com>
2024-04-28 16:45:27 +0530

    mobile nav

--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -3,13 +3,17 @@ import { ChevronDownOutline } from 'react-ionicons'
 import { Link } from 'react-router-dom';
 import { urlRoutes } from '../../routes';
 import logo from "./../../assets/logo.svg";
+import classnames from "classnames";
 
 function Navbar() {
    const [state, setState] = useState(false);
+   const [submenu, setSubmenu] = useState("");
+   console.log(submenu, "submenu");
    const handleNav=()=>{
     setState((prev)=> !prev);
-
    }
+
+  
 
   
 
@@ -112,62 +116,104 @@ function Navbar() {
         </div>
 
         <button
-          className="flex flex-col items-center justify-center w-12 h-10 border-2 border-primary-light bg-primary-light hover:bg-primary rounded-md"
-          onClick={handleNav}
-        >
-          <span className="block w-5 h-0.5 bg-white"></span>
-          <span className="block w-5 h-0.5 bg-white mt-1"></span>
-          <span className="block w-5 h-0.5 bg-white mt-1"></span>
-        </button>
+  className="md:hidden flex flex-col p-3 items-center justify-center w-12 h-10 border-2 border-primary-light bg-primary-light hover:bg-primary rounded-md"
+  onClick={handleNav}
+>
+  <span className={classnames("block w-full h-0.5 bg-white transition-transform origin-top-center", {"rotate-45 translate-y-1": state})}></span>
+  <span className={classnames("block w-full h-0.5 bg-white m-1 transition-opacity", {"opacity-0 my-0": state,})}></span>
+  <span className={classnames("block w-full h-0.5 bg-white transition-transform origin-bottom-center", {" -rotate-45 ": state})}></span>
+</button>
+{/* <button class="w-14 h-14 relative focus:outline-none bg-teal-600 rounded" >
+                <div class="block w-5 absolute left-6 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
+                    <span  class="block absolute h-0.5 w-7 text-white bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
+                    <span  class="block absolute  h-0.5 w-5 text-white bg-current   transform transition duration-500 ease-in-out" :class="{'opacity-0': open } "></span>
+                    <span  class="block absolute  h-0.5 w-7 text-white bg-current transform  transition duration-500 ease-in-out" :class="{'-rotate-45': open, ' translate-y-1.5': !open}"></span>
+                </div>
+            </button> */}
+
       </div>
 
-      {state ? (
-        <nav className="bg-primary w-full  absolute top-24 left-0 z-10 transition-transform duration-300 scale-0">
-          <ul className="flex flex-col items-center pt-16">
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                Home
-              </a>
+      {/* mobile menu */}
+      <nav className= {classnames('bg-primary w-full px-4 min-h-max pb-10   absolute top-24 left-0 z-10 transition-transform duration-300 origin-top scale-y-0 md:hidden', {"scale-y-100": state})}>
+          <ul className="flex flex-col  pt-16">
+          <li className="text-white p-4 min-w-max">
+              <Link to={urlRoutes.HOME}>Home</Link>
             </li>
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                About
-              </a>
+            <li className="text-white p-4 min-w-max">
+              <Link to={urlRoutes.ABOUT}>About</Link>
             </li>
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                Contact
-              </a>
+            <li className="text-white relative p-4  min-w-max" onClick={()=> setSubmenu(prev=> prev? null: 1)}>
+              <Link to={urlRoutes.SERVICE}>
+                <span>
+                  Services
+                  <ChevronDownOutline
+                    color={"#fff"}
+                    width={"15px"}
+                    height={"15px"}
+                    cssClasses={"inline mx-2 mb-1"}
+                  />
+                </span>
+              </Link>
+              <ul
+                className={ classnames(`mt-4
+                    rounded-b-lg z-10 bg-primary shadow-sm
+                      border-t-primary-light border-t-4 min-w-max
+                      transition-all duration-500
+                      origin-top 
+                      `, {
+                        "scale-y-100 opacity-1 h-auto": submenu ==1,
+                        "scale-y-0 opacity-0 h-0 overflow-hidden": submenu!=1,
+                      })}
+              >
+                <li className=" text-white p-4 border-b-[1px] border-b-cyan-900 group-hover:border-b-0 transition-all delay-100 ease-out duration-300 ">
+                  <Link to={urlRoutes.SERVICE}>Services</Link>
+                </li>
+                <li className="text-white p-4 border-b-[1px] border-b-cyan-900 group-hover:border-b-0 transition-all delay-100 ease-out duration-300">
+                  <Link to={urlRoutes.SERVICE_DETAIL}>Service Detail</Link>
+                </li>
+              </ul>
             </li>
 
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                Home
-              </a>
+            <li className="text-white relative group p-4 min-w-max" onClick={()=> setSubmenu(prev=> prev? null: 2)}>
+              <Link to={urlRoutes.SERVICE}>
+                <span>
+                  Cases
+                  <ChevronDownOutline
+                    color={"#fff"}
+                    width={"15px"}
+                    height={"15px"}
+                    cssClasses={"inline mx-2 mb-1"}
+                  />
+                </span>
+              </Link>
+              <ul
+                 className={ classnames(`mt-4
+                    rounded-b-lg z-10 bg-primary shadow-sm
+                      border-t-primary-light border-t-4 min-w-max
+                      transition-all duration-500
+                      origin-top 
+                      `, {
+                        "scale-y-100 opacity-1 h-auto": submenu ==2,
+                        "scale-y-0 opacity-0 h-0 overflow-hidden": submenu!=2,
+                      })}
+              >
+                <li className=" text-white p-4 border-b-[1px] border-b-cyan-900 group-hover:border-b-0 transition-all delay-100 ease-out duration-300 " >
+                  <Link to={urlRoutes.CASES}>Cases</Link>
+                </li>
+                <li className="text-white p-4 border-b-[1px] border-b-cyan-900 group-hover:border-b-0 transition-all delay-100 ease-out duration-300">
+                  <Link to={urlRoutes.CASE_DETAIL}>Case Detail</Link>
+                </li>  
+              </ul>
             </li>
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                About
-              </a>
+
+            <li className="text-white p-4 min-w-max">
+              <Link to={urlRoutes.TEAMS}>Teams</Link>
             </li>
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="py-2 px-4 block hover:bg-gray-200">
-                Contact
-              </a>
+            <li className="text-white p-4 min-w-max">
+              <Link to={urlRoutes.CONTACT}>Contact Us</Link>
             </li>
           </ul>
         </nav>
-      ) : null}
     </div>
   );
 }
--- src/data/teams.jsx
+++ src/data/teams.jsx
@@ -3,14 +3,16 @@ export const akritiImage = akriti
 export const teamsData = [
   {
     slug: "akrity",
-    name: "Akriti",
+    name: "Akriti Elhence",
     designation: "Consultant",
     quote:
       "I provide valuable insights into the intricacies of\
     taxation systems and accounting practices, highlighting both the similarities and differences\
     between India and the GCC countries",
     profilePic: akriti,
-    desc: `With over eight years of extensive experience in taxation and accounting across both India and
+    description: 
+      <p className="text-secondary">
+      With over eight years of extensive experience in taxation and accounting across both India and
     the GCC (Gulf Cooperation Council), I serves as a comprehensive resource hub for businesses
     and individuals navigating the complexities of financial management in these regions. My
     expertise encompasses a wide range of areas, from tax compliance and regulatory
@@ -23,52 +25,9 @@ export const teamsData = [
     and client testimonials showcase our practical knowledge and proven track record in helping
     businesses thrive amidst evolving fiscal landscapes. Whether you&#39;re seeking expert advice,
     practical solutions, or industry-leading insights, I will be your go-to destination for all your
-    taxation and accounting needs in India and the GCC.`,
-    description: (
-      <>
-        <p className="text-secondary my-2">
-          I am ACCA licensed and have over 20 years’ experience in Accounting
-          and Strategic Finance with different companies in Oman and East
-          Africa. I have also qualified as Strategic CFO in Strategic CFO
-          Program 2022 with MECA CFO ACADEMY.
-        </p>
-        <p className="text-secondary my-2">
-          We absolutely love assisting individuals like yourself in various
-          areas through our Personal Development Program, as well as providing
-          guidance in Accounting & Finance and Strategic Finance Coaching.
-          Additionally, we offer a specialized program called Pure Strategic
-          Mastery.
-        </p>
-        <p className="text-secondary my-2">
-          We offer a specialized course designed to assist individuals in
-          interview preparation and securing positions in the finance industry
-          or higher-level roles. This unique program ensures that you have the
-          necessary knowledge and experience to confidently enter the
-          Professional World. This program is under the Personal Development
-          Program.
-        </p>
-        <p className="text-secondary my-2">
-          We have helped numerous students and professionals find their ideal
-          career paths. Feel free to reach out with any questions or concerns
-          you have about your own career journey. We can work together
-          one-on-one to address any professional challenges you may be facing.
-        </p>
-        <p className="text-secondary my-2">
-          You can access a variety of free templates to assist you in your
-          studies, work, or professional development. Feel free to download them
-          from{" "}
-          <a className="text-info font-extrabold" href="https://linkedin.com/in/iman-hamdan-acca-657045118">
-            linkedin.com/in/iman-hamdan-acca-657045118
-          </a>
-        </p>
-        <p className="text-secondary my-2">
-          You can stay updated with more tips and insights in those areas by
-          following me on{" "}
-          <a className="text-info font-extrabold" href="https://linkedin.com/in/iman-hamdan-acca-657045118">LinkedIn</a>.
-          Feel free to connect with me there.
-        </p>
-      </>
-    ),
+    taxation and accounting needs in India and the GCC.
+      </p>
+
   },
   {
     slug: "iman",
